05200cc3266248d5d1fef02ab5a0494b8a8f234c
Makman2 <makman@alice.de>
2016-10-22 01:00:33 +0200

    pack_flink.sh: Make copy verbose

--- pack_flink.sh
+++ pack_flink.sh
@@ -1,6 +1,6 @@
 set -e
 
 mkdir -p flink/flink-1.2-SNAPSHOT
-cp -a ~/dev/makman2-flink/build-target/. ~/dev/makman2-flink-mesos-demo/flink/flink-1.2-SNAPSHOT/.
+cp -v -a ~/dev/makman2-flink/build-target/. ~/dev/makman2-flink-mesos-demo/flink/flink-1.2-SNAPSHOT/.
 cd ~/dev/makman2-flink-mesos-demo/flink
 tar -cvzf flink-1.2-DEV-SNAPSHOT-bin-hadoop2.tar.gz flink-1.2-SNAPSHOT
