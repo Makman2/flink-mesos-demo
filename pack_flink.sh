aa6cfdfb2a540e6b84353208583d21cdf42acf52
Makman2 <makman@alice.de>
2016-10-26 15:50:42 +0200

    WIP - latest experiments

--- pack_flink.sh
+++ pack_flink.sh
@@ -4,3 +4,6 @@ mkdir -p flink/flink-1.2-SNAPSHOT
 cp -v -a ~/dev/makman2-flink/build-target/. ~/dev/makman2-flink-mesos-demo/flink/flink-1.2-SNAPSHOT/.
 cd ~/dev/makman2-flink-mesos-demo/flink
 tar -cvzf flink-1.2-DEV-SNAPSHOT-bin-hadoop2.tar.gz flink-1.2-SNAPSHOT
+cd ..
+docker build -t makman2/flink-mesos-demo:dev .
+docker push makman2/flink-mesos-demo
